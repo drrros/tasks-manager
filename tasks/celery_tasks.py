f82605db16bdedd68016604b732a0fff2e20a2f3
DrRos <admin@drros.ru>
2020-07-01 23:26:31 +0400

    send mail

--- tasks/celery_tasks.py
+++ tasks/celery_tasks.py
@@ -1,4 +1,7 @@
+import smtplib
+
 from celery import shared_task
+from django.core.mail import send_mail
 
 from .models import CeleryTask
 
@@ -8,19 +11,23 @@ from .models import CeleryTask
 
 
 @shared_task
-def sendemail(recipient):
+def sendemail(recipient, task_header, task_content, task_date, task_type):
     try:
         # https://github.com/celery/celery/issues/4300
         celery_task = CeleryTask.objects.get(celery_task_id=sendemail.request.id)
         if not celery_task.completed:
             print('task: sendemail, recepient:' + recipient)
-            # send_mail(
-            #     'Subject here',
-            #     'Here is the message.',
-            #     'from@example.com',
-            #     [recipient,],
-            #     fail_silently=False,
-            # )
+            try:
+                send_mail(
+                    f'Напоминание о событии: {task_header}',
+                    f'Событие {task_header} ({task_content}) - {task_type} начнётся через 1 час в {task_date}',
+                    'notify@domain.ru',
+                    [recipient,],
+                    fail_silently=False,
+                )
+            except smtplib.SMTPException as e:
+                pass
+                # TODO: make smth with it (retry?)
             celery_task.completed = True
             celery_task.save()
             return True
--- tasks_manager/settings.py
+++ tasks_manager/settings.py
@@ -147,3 +147,8 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Europe/Volgograd'
 CELERY_IMPORTS = ('tasks.celery_tasks' )
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "localhost")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", "25")
